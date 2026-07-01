52e37e46281f0d283fc78467b16958c8d97fadae
febryardiansyah <febrymuhammad80@gmail.com>
2026-07-01 19:15:18 +0700

    Initial Commit

---
+++ openrouter_app_swift/openrouter_app_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  openrouter_app_swiftApp.swift
+//  openrouter_app_swift
+//
+//  Created by Bank Indonesia on 01/07/26.
+//
+
+import SwiftUI
+
+@main
+struct openrouter_app_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
